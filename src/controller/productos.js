1ef622713903b60491f2002ac61b63a16e0679e1
Laura Olaya <laurasofiaolayaisaza@gmail.com>
2022-10-18 20:59:50 -0500

    first commit

---
+++ src/controller/productos.js
@@ -0,0 +1,105 @@
+var createError = require('http-errors')
+const fs = require('fs/promises');
+const path = require('path');
+
+const filePath = path.resolve(__dirname, '../../productos.json');
+
+class ProductosAPI {
+    constructor(archivo) {
+        this.archivo = archivo;
+    }
+
+    async exists(id) {
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos);
+        const indice = arrayProductos.findIndex(prod => prod.id == id);
+
+        return indice >= 0;
+    }
+
+    async getAll() {
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos)
+        return arrayProductos
+    }
+
+    async getById(id) {
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos);
+        const existe = await this.exists(id)
+
+        if(!existe) throw createError(404, 'producto no encontrado')
+
+        const indice = arrayProductos.findIndex(prod => prod.id == id);
+
+        return arrayProductos[indice]
+    }
+
+    async saveNewProduct(newProduct) {
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos)
+
+        const {title, price} = newProduct
+
+        let newId = 1
+
+        if(arrayProductos.length) {
+           newId = arrayProductos[arrayProductos.length - 1].id + 1
+        }
+
+        const intId = Math.floor(newId)
+    
+        const product = {
+            title,
+            price,
+            id: intId
+        }
+
+        arrayProductos.push(product);
+
+        const newData = JSON.stringify(arrayProductos, null, "\t")
+
+        await fs.writeFile(filePath, newData)
+            return product
+        }
+
+    async updateById (id, newProduct) {
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos)
+        const indice = arrayProductos.findIndex(prod => prod.id == id);
+
+        const {title, price} = newProduct
+
+        const productoActualizado = {
+            title,
+            price,
+            id: id,
+        }
+
+        arrayProductos.splice(indice, 1, productoActualizado);
+
+        const DataActualizada = JSON.stringify(arrayProductos, null, "\t")
+        await fs.writeFile(filePath, DataActualizada)
+
+        return productoActualizado
+    }
+
+    async deleteById (id){
+        const productos = await fs.readFile(filePath, 'utf8');
+        const arrayProductos = JSON.parse(productos)
+        const indice = arrayProductos.findIndex(prod => prod.id == id);
+
+        arrayProductos.splice(indice, 1);
+
+        const newData = JSON.stringify(arrayProductos, null, "\t")
+        await fs.writeFile(filePath, newData)
+        
+        return `eliminando el producto con el id: ${id}`
+    }
+}
+
+const instanciaProductosApi = new ProductosAPI(filePath);
+
+module.exports = {
+    ProductosController : instanciaProductosApi
+}
